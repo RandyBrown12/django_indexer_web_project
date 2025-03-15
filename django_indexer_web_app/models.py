685ee4f0f3d02758b3e120749b3203e6ec6401bb
Randy <randybrown9812@gmail.com>
2025-03-15 12:40:16 -0500

    Create schema for django and website to use

--- django_indexer_web_app/models.py
+++ django_indexer_web_app/models.py
@@ -2,13 +2,25 @@ from django.db import models
 
 # Create your models here.
 class Block(models.Model):
+    block_id = models.UUIDField(primary_key=True)
     block_hash = models.CharField(null=False)
     chain_id = models.CharField(null=False)
     created_at = models.DateTimeField(null=False)
-    height = models.IntegerField(null=False, primary_key=True)
+    height = models.IntegerField(null=False)
     tx_num = models.IntegerField(null=False)
 
     class Meta:
         managed = False
         db_table = 'blocks'
-        unique_together = (('chain_id', 'height'), ('chain_id', 'block_hash', 'height'),)
+
+class Transaction(models.Model):
+    tx_id = models.UUIDField(primary_key=True)
+    tx_hash = models.CharField(null=False)
+    height = models.CharField(null=False)
+    fee_amount = models.CharField(null=False)
+    fee_denom = models.CharField(null=False)
+    created_at = models.DateTimeField(null=False)
+
+    class Meta:
+        managed = False
+        db_table = 'transactions'
--- django_indexer_web_app/views.py
+++ django_indexer_web_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Block
+from .models import Block, Transaction
 # Create your views here.
 def index_page(request):
     return render(request, 'index.html',)
@@ -10,4 +10,5 @@ def blocks_page(request):
     return render(request, 'blocks.html', {'blocks': blocks})
 
 def transactions_page(request):
-    return render(request, 'transactions.html')
+    transactions = Transaction.objects.all()
+    return render(request, 'transactions.html', {'transactions': transactions})
